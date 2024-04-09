66dcddd7740a8a2f785c290fddedceb3570b93f2
Teeraphol Panpuvapat <teeraphol.panpuvapat@metropolia.fi>
2024-04-09 18:53:27 +0300

    all test passed

--- src/api/controllers/uploadController.ts
+++ src/api/controllers/uploadController.ts
@@ -2,4 +2,33 @@
 // to the client
 // data to send is described in UploadMessageResponse interface
 
-// export {catPost};
+import {Point} from 'geojson';
+import {Request, Response, NextFunction} from 'express';
+import CustomError from '../../classes/CustomError';
+
+const catPost = async (
+  req: Request<{}, {}, {}, {}, {coords: Point}>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (!req.file) {
+      const err = new CustomError('File not valid', 400);
+      throw err;
+    }
+
+    const response = {
+      message: 'cat uploaded',
+      data: {
+        filename: req.file.filename,
+        location: res.locals.coords,
+      },
+    };
+
+    res.json(response);
+  } catch (error) {
+    next(new CustomError((error as Error).message, 400));
+  }
+};
+
+export {catPost};
